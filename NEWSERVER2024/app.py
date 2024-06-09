0d92b8968fadb1585ed6925f274684113632ae8e
Joe C <38189554+Praxiliphicuz@users.noreply.github.com>
2024-06-09 11:55:40 -0700

    Add files via upload

---
+++ NEWSERVER2024/app.py
@@ -0,0 +1,9 @@
+#create a flask virtenvi for this to work
+from flask import Flask
+from views import views
+
+app = Flask (__name__)
+app.register_blueprint(views, url_prefix="/views")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
